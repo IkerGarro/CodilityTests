da7e4a34b997955b7d52fb37e874813d50636218
Iker <iker@Ikers-MacBook-Air.local>
2025-03-15 11:07:10 +0100

    Add Missing PassingCars test

--- src/main/java/codility/easy/PassingCars.java
+++ src/main/java/codility/easy/PassingCars.java
@@ -1,4 +1,21 @@
 package codility.easy;
 
 public class PassingCars {
+    public int solution(int[] A) {
+        int numberOfOnes = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 1) {
+                ++numberOfOnes;
+            }
+        }
+        int numberOfPairs = 0;
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] == 0) {
+                numberOfPairs += numberOfOnes;
+            } else {
+                numberOfOnes--;
+            }
+        }
+        return numberOfPairs;
+    }
 }
--- src/test/java/codility/easy/PassingCarsTest.java
+++ src/test/java/codility/easy/PassingCarsTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package codility.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class PassingCarsTest {
-  
+
+    PassingCars passingCars = new PassingCars();
+
+    @Test
+    void a() {
+        assertAll(() -> {
+                    assertEquals(5, passingCars.solution(new int[]{0, 1, 0, 1, 1}));
+                    assertEquals(2, passingCars.solution(new int[]{1, 1, 0, 1, 1}));
+                    assertEquals(0, passingCars.solution(new int[]{1, 1, 1, 1, 0}));
+                    assertEquals(4, passingCars.solution(new int[]{0, 1, 1, 1, 1}));
+                    assertEquals(1, passingCars.solution(new int[]{1, 1, 1, 0, 1}));
+                }
+        );
+    }
 }
